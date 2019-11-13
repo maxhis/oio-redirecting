fd2f5900520fa8f9798b755b0a2fd4ee56ae34aa
iStar <istar309@gmail.com>
2019-11-13 12:23:53 +0800

    Add ssh renew script

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f /home/ubuntu/oio-redirecting/docker-compose.yml run certbot renew --dry-run \
+/usr/local/bin/docker-compose -f /home/ubuntu/oio-redirecting/docker-compose.yml run certbot renew \
 && /usr/local/bin/docker-compose -f /home/ubuntu/oio-redirecting/docker-compose.yml kill -s SIGHUP webserver
